c9bcf9c5af50915e2ceee70f21bb267a8143249f
AlexeyKM <kmayonezalex@gmail.com>
2022-12-04 00:32:34 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            if (a < b)
+            {
+                Console.WriteLine(b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
